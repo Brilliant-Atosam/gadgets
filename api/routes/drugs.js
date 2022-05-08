76ed141247e54b191d6b9272e56be2ab8585945f
Brilliant Atosam <atosam91@gmail.com>
2022-05-08 08:31:04 +0100

    Server basics

--- api/routes/drugs.js
+++ api/routes/drugs.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
+// const { set } = require("mongoose");
 const Drug = require("../models/Drug");
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: "./public/drugs",
+  filename: (req, file, cb) => {
+    cb(null, req.body.name.replace(" ", "_") + path.extname(file.originalname));
+  },
+});
+const uploadDrug = multer({
+  storage,
+}).single("drug");
 // GET ALL DRUGS
 router.get("/", async (req, res) => {
   try {
@@ -10,31 +22,63 @@ router.get("/", async (req, res) => {
   }
 });
 // GET SINGLE DRUG
-router.get("/:id", async (req, res) => {});
-// ADD
-router.post("/", async (req, res) => {
-  const { name, stock, supplier, dosage, implications, price, img } = req.body;
-  console.log(req.body);
+router.get("/:id", async (req, res) => {
   try {
-    const newDrug = new Drug({
-      name,
-      stock,
-      supplier,
-      implications: implications.split(", "),
-      dosage,
-      price,
-      img,
-      id: 1,
-    });
-    await newDrug.save();
-    res.status(200).json(newDrug);
+    const drug = await Drug.findOne({ id: req.params.id });
+    drug
+      ? res.json(drug)
+      : res.status(404).json(`Drug with id '${req.params.id}' not found.`);
   } catch (err) {
-    res.status(500).json("Something went wrong!");
+    res.status(500).json("Oooops! Please try again.");
+  }
+});
+// ADD
+router.post("/", (req, res) => {
+  uploadDrug(req, res, async (err) => {
+    if (err || req.file === undefined)
+      req.status(500).json("Oooops! Try again.");
+    else {
+      console.log(req.file);
+      const { name, stock, supplier, dosage, implications, price, img } =
+        req.body;
+      try {
+        const newDrug = new Drug({
+          name,
+          stock,
+          supplier,
+          implications: implications.split(", "),
+          dosage,
+          price,
+          img: req.file.filename,
+          id: Math.floor(1000 + Math.random() * 9000),
+        });
+        await newDrug.save();
+        res.status(200).json(newDrug);
+      } catch (err) {
+        res.status(500).json("Something went wrong!");
+        console.log(err.message);
+      }
+    }
+  });
+});
+// EDIT DRUG
+router.put("/:id", async (req, res) => {
+  try {
+    const drug = await Drug.findOne({ id: req.params.id });
+    await drug.updateOne({ $set: req.body }, { new: true });
+    res.json("Updated successfully");
+  } catch (err) {
+    res.status(500).json("Oooops! Try again");
     console.log(err.message);
   }
 });
-// EDIT DRUG
-router.put("/", async (req, res) => {});
 // DELETE DRUG
-router.delete("/", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    await Drug.findOneAndDelete({ id: req.params.id });
+    res.json("Deletion successful!");
+  } catch (err) {
+    res.status(500).json("Oooops! Try again");
+  }
+});
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -17,20 +17,14 @@ server.use("static", express.static("public"));
 // CORS SETTINGS
 server.use(cors());
 // UPLOAD
-const storage = multer.diskStorage({
-  destination: "./public/drugs",
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + Date.now() + path.extname(file.originalname));
-  },
-});
-const uploadDrug = multer({
-  storage,
-});
+
 // routes
-server.post("/", uploadDrug.single("drug"), (req, res) => {
+server.post("/", (req, res) => {
   res.send("Hello Philomina Fosua");
 });
 server.use("/auth", require("./routes/auth"));
 server.use("/drugs", require("./routes/drugs"));
-console.log(bcrypt.hashSync("philo", 10));
+console.log(
+  (Math.floor(Math.random() * 10000) + 10000).toString().substring(1)
+);
 server.listen(process.env.PORT || 3000, () => console.log("Server is running"));
