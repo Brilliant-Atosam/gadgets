a34985373b9fd342b3ed7b7199877cf82aa846a1
Brilliant-Atosam <atosam91@gmail.com>
2022-05-31 17:58:15 +0100

    rebuilt

--- client/src/request.js
+++ client/src/request.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API_URL = "http://localhost:8001/";
+// const API_URL = "http://localhost:8001/";
+const API_URL = "http://gadgetapi.bongostores.shop/";
 export const request = axios.create({
   baseURL: API_URL,
 });
