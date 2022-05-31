fc8a85fda0d930cc80552799b9668cdabefbd2f7
Brilliant-Atosam <atosam91@gmail.com>
2022-05-31 22:36:41 +0100

    rebuilt

--- api/server.js
+++ api/server.js
@@ -1,22 +1,16 @@
 const mongoose = require("mongoose");
 const cors = require("cors");
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const multer = require("multer");
 const path = require("path");
 const server = express();
-const router = require("./routes/auth");
-const moment = require("moment");
 require("dotenv").config();
 // DB CONNECTION
-
 mongoose.connect(process.env.DB_CONNECTION_STRING, (err) => {
   err ? console.log(err.message) : console.log("Connected to DB");
 });
 server.use(express.json());
 // CORS SETTINGS
 server.use(cors());
-
 server.use("/auth", require("./routes/auth"));
 server.use("/store", require("./routes/store"));
 server.use("/devices", require("./routes/devices"));
